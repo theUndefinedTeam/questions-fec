89536f8a217500e975f8007e2e90981bec08d0cd
Kelvin B. Davis <archidit7@gmail.com>
2020-11-12 15:49:57 -0700

    added basic setup of file

---
+++ client/src/components/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import '../main.css';
+const App = () => (
+  <div>
+    <body className="gl-body">
+      <Container>
+        <h1> Questions and Answers</h1>
+
+        <Row>
+          <Col>
+
+          </Col>
+        </Row>
+      </Container>
+    </body>
+  </div>
+);
+
+export default App;
