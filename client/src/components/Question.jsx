cdebd95262087f6d5131dbb3fcd1e8e26e39543f
Kelvin B. Davis <archidit7@gmail.com>
2020-11-16 15:07:16 -0700

    added components for answers, will begin working on those

--- client/src/components/Question.jsx
+++ client/src/components/Question.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { format, formatRelative } from 'date-fns'
 const Question = (props) => {
     return (
         <>
@@ -15,19 +16,18 @@ const Question = (props) => {
                                 `  Q:  ${props.questions.question_body}`
                             }
                         </div>
+                        <Col> <small>by  {props.questions.asker_name},  {format(new Date(props.questions.question_date), 'MMMM dd, yyyy')} </small> </Col>
+                    </Col>
+                    <Col>
                         <Col>
-
+                            <div className="gl-answers">
+                                {
+                                    `  A:  Answer will go heere`
+                                }
+                            </div>
                         </Col>
                     </Col>
-                    <Col>
-                    </Col>
-                    <Col>
-                        <div className="gl-answers">
-                            {
-                                `  A:  Answer will go heere`
-                            }
-                        </div>
-                    </Col>
+
 
 
                 </Row>
