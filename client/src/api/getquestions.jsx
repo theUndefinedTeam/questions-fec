de02204715a38e791534d9279b2cb531bfe0765c
Kelvinbdavis <37672442+Kelvinbdavis@users.noreply.github.com>
2020-11-23 12:33:42 -0700

    fixed changes

--- client/src/api/getquestions.jsx
+++ client/src/api/getquestions.jsx
@@ -1,11 +1,6 @@
 const axios = require('axios');
-<<<<<<< HEAD
-//d
-const getProductQuestionGet = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
-=======
 
-const getProductQuestions = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
->>>>>>> 3fe6584884da7b45a5ddd2236f951edd1c19d236
+const getProductQuestionGet = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
   .then((result) => result)
   .catch((err) => { throw err; });
 
