6556a75fe71eb7291d1b81cbe28beeb270972632
Kelvinbdavis <37672442+Kelvinbdavis@users.noreply.github.com>
2020-11-23 12:38:13 -0700

    next chagne

--- client/src/api/getquestions.jsx
+++ client/src/api/getquestions.jsx
@@ -1,5 +1,5 @@
 const axios = require('axios');
-
+//d
 const getProductQuestionGet = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
   .then((result) => result)
   .catch((err) => { throw err; });
