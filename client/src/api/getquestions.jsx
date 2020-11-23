fb2d43b532646a54b69728946774fda08acc4b61
Kelvinbdavis <37672442+Kelvinbdavis@users.noreply.github.com>
2020-11-23 16:33:35 -0700

    fixed small error in naming

--- client/src/api/getquestions.jsx
+++ client/src/api/getquestions.jsx
@@ -1,6 +1,6 @@
 const axios = require('axios');
 //d
-const getProductQuestionGet = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
+const getProductQuestions = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
   .then((result) => result)
   .catch((err) => { throw err; });
 
